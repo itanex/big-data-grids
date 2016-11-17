75bcee037f3c3f4e3ce654fbcd2f8fbc84c96901
Bryan Wood <itanex@gmail.com>
2016-11-17 10:13:36 -0800

    Remove unused ng-bootstrap dependency

--- scripts/app.ts
+++ scripts/app.ts
@@ -4,7 +4,6 @@ module BigDataGrids {
       'app.bigData', // My Application's Home Module
       'app.uiVirtualList', // Custom ui-virtual-list directive
       'ngSanitize',
-      'ui.bootstrap', // Angular UI Bootstrap
       'ui.grid', // Angular UI Grid
       'sf.virtualScroll' // vendor/sf-virtual-scroll.js
     ]);
